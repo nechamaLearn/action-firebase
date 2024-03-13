7a486a3e72e9ce4e54782095fc183e02c28cb51b
nnn <y@y.com>
2024-03-13 16:05:37 +0200

    test pre

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,23 @@ function App() {
   return (
     <>
       <div>
-        <a href='https://vitejs.dev' target='_blank'>
+        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
           <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
-        <a href='https://react.dev' target='_blank'>
+        <a href='https://react.dev' target='_blank' rel='noreferrer'>
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className='card'>
-
-
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR Edit <code>src/App.jsx</code> and save to test HMR Edit{' '}
-          <code>src/App.jsx</code> and save to test HMR Edit <code>src/App.jsx</code> and save to test HMR Edit{' '}
+          Edit <code>src/App.jsx</code> and save to test HMR Edit{' '}
+          <code>src/App.jsx</code> and save to test HMR Edit{' '}
+          <code>src/App.jsx</code> and save to test HMR Edit{' '}
+          <code>src/App.jsx</code> and save to test HMR Edit{' '}
           <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
