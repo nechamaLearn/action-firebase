fd821778d0af0c5fbf141fdc5d3c5f793c65bdd6
nnn <y@y.com>
2024-04-03 13:20:42 +0300

    ccc

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    after my deploy
       <h1>Vite + React</h1>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
